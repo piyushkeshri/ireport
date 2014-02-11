cf5ae7e884e9e03b7655b6dccc5eb1cfe0bc0772
Rohini B <rohiniab@gmail.com>
2014-02-10 19:02:03 -0800

    layout templates

---
+++ templates/header.php
@@ -0,0 +1,65 @@
+<html>
+<head>
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="template.css">
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+
+
+	<title><?php echo $title ?> - CodeIgniter 2 Tutorial</title>
+	<div class="container">
+  
+    
+      
+        <div class="row-fluid">
+            <div class="span12">
+                <div class="span6">
+                    <h1 class="muted">Company Name</h1>
+                </div>
+
+                
+            </div>
+        </div>
+
+        <div class="navbar">
+            <div class="navbar-inner">
+                <div id="nav" class="container">
+                    <ul id="navlist"class="navL">
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Profile</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Report</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Contact Us</a>
+                        </li>
+                    </ul>
+
+                    <div id ="Login">
+                        <a href="#">Log In</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+           
+
+      
+    
+  
+</div>
+</head>
+<body>
+	<div class="container">
+  
+    
+      
+        <div class="row-fluid">
+            <div class="span12">
+                <div class="span6">
---
+++ templates/home.php
@@ -0,0 +1,24 @@
+<?php include 'header.php'; ?>
+
+<div>
+	<img src="http://geoawesomeness.com/wp-content/uploads/2013/10/lidar1.jpg" alt="Smiley face" height="300" width="700">
+</div>
+
+<div >
+    
+       
+    <ul id="navlist"class="navL">
+        <li>
+        	<button id = "Btn_Report" type="button">Report</button>
+    	</li>
+		<li>
+        	<button id = "Btn_Search" type="button">Search</button>
+    	</li>
+    	<li>
+        	<button id = "Btn_Vote" type="button">Vote</button>
+    	</li>
+    </ul>
+</div>
+
+
+<?php include 'footer.php'; ?>
